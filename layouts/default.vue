698fe96678df1d1612a3c22a7ad1aedc73938bf1
AdamPawlinski <apawlinski@vuestorefront.io>
2023-10-31 22:14:45 +0100

    feat: layouts and login ui adjust

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="container">
-    <CThemeProvider>
-      <CColorModeProvider>
-        <CBox font-family="body" as="main">
-          <CReset />
-          <Nuxt />
-        </CBox>
-      </CColorModeProvider>
-    </CThemeProvider>
-  </div>
+  <UContainer>
+    <UCard>
+      <template #header>
+      </template>
+      <Nuxt />
+      <template #footer>
+      </template>
+    </UCard>
+  </UContainer>
 </template>
-<script>
-import {
-  CThemeProvider,
-  CColorModeProvider,
-  CReset,
-  CBox
-} from '@chakra-ui/vue'
+<script setup lang="ts">
 
-export default {
-  name: 'DefaultLayout',
-  components: {
-    CThemeProvider,
-    CColorModeProvider,
-    CReset,
-    CBox
-  }
-}
+
+
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,32 @@
 <template>
   <div>
-    <CBox 
+    <div 
       my="3"
       d="flex"
       justify-content="flex-end"
     >
-      <CButton mr="3" @click="onClickStartLotteryHandler(false)">
+      <UButton mr="3" @click="onClickStartLotteryHandler(false)">
         Start lottery
-      </CButton>
-      <CIconButton
-        mr="3"
-        :icon="colorMode === 'light' ? 'moon' : 'sun'"
-        :aria-label="`Switch to ${
-          colorMode === 'light' ? 'dark' : 'light'
-        } mode`"
-        @click="toggleColorMode"
-      />
-    </CBox>
-    <CBox
-      v-bind="mainStyles[colorMode]"
+      </UButton>
+    </div>
+    <div
       d="flex"
       w="100vw"
       h="95vh"
       flex-dir="column"
       justify-content="center"
     >      
-      <CHeading text-align="center" mb="4">
+      <div text-align="center" mb="4">
         Christmas gifts lottery
-      </CHeading>
-      <CFlex justify="center" direction="column" align="center">
-        <CFormControl is-required >
-          <CFormLabel for="fname">Imię</CFormLabel>
-          <CInput id="fname" placeholder="First name" @change.prevent="setName"/>
+      </div>
+      <div class="flex flex-col justify-center content-center">
+        <UForm>
+          <UFormGroup label="Imię" name="name">
+            <UInput id="name" placeholder="First name" @change.prevent="setName"/>
+          </UFormGroup>
           <!-- <CFormLabel for="sname">Nazwisko</CFormLabel>
           <CInput id="sname" placeholder="Last name" /> -->
-          <CButton
+          <UButton
             loading-text="Submitting"
             variant-color="blue"
             variant="outline"
@@ -43,145 +35,97 @@
             @click="onClickSubmitHandler"  
           >
             Sprawdź kogo wylosowałeś
-          </CButton>
-        </CFormControl>
+          </UButton>
+        </UForm>
         
-        <CModal :is-open="showModalLottery">
-          <CModalOverlay />
-          <CModalContent v-if="!startLottery">
-            <CModalHeader>Czy chcesz rozpocząć nowe losowanie?</CModalHeader>
-            <CModalBody>
-              <CButton @click="onClickStartLotteryHandler(true)">
+        <UModal :is-open="showModalLottery">
+          <UCard v-if="!startLottery">
+            <template #header>
+              Czy chcesz rozpocząć nowe losowanie?
+            </template>
+              <UButton @click="onClickStartLotteryHandler(true)">
                 Tak
-              </CButton>
+              </UButton>
               
-              <CButton @click="showModalLottery = false">
+              <UButton @click="showModalLottery = false">
                 Nie
-              </CButton>
-            </CModalBody>
-            <CModalFooter>
-              <CButton @click="showModalLottery = false">
+              </UButton>
+            <template #footer>
+              <UButton @click="showModalLottery = false">
                 Cancel
-              </CButton>
-            </CModalFooter>
-            <CModalCloseButton @click="showModalLottery = false" />
-          </CModalContent>
-          <CModalContent v-else>
-            <CModalHeader>The lottery is finished</CModalHeader>
-            <CModalFooter>
-              <CButton @click="showModalLottery = false; startLottery = false">
+              </UButton>
+            </template>
+            <UButton @click="showModalLottery = false" />
+          </UCard>
+          <UCard v-else>
+            <template #header>
+              The lottery is finished 
+            </template>
+            <template #footer>
+              <UButton @click="showModalLottery = false; startLottery = false">
                 Cancel
-              </CButton>
-            </CModalFooter>
-            <CModalCloseButton @click="showModalLottery = false; startLottery = false" />
-          </CModalContent>
-        </CModal>
-        <CModal :is-open="showModalResult">
-          <CModalOverlay />
-          <CModalContent>
-            <CModalHeader>Wylosowałaś/eś</CModalHeader>
-            <CModalBody>{{drawPick}}</CModalBody>
-            <CModalFooter>
-              <CButton @click="showModalResult = false">
+              </UButton>
+            </template>
+            <UButton @click="showModalLottery = false; startLottery = false" />
+          </UCard>
+        </UModal>
+        <UModal :is-open="showModalResult">
+          <UCard>
+            <template #header>
+              Wylosowałaś/eś
+            </template>
+            <div>{{drawPick}}</div>
+            <template #footer>
+              <UButton @click="showModalResult = false">
                 Cancel
-              </CButton>
-            </CModalFooter>
-            <CModalCloseButton @click="showModalResult = false" />
-          </CModalContent>
-        </CModal>
-      </CFlex>
-    </CBox>
+              </UButton>
+            </template>
+            <UButton @click="showModalResult = false" />
+          </UCard>
+        </UModal>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="js">
-import {
-  CBox,
-  CButton,
-  CModal,
-  CModalContent,
-  CModalOverlay,
-  CModalHeader,
-  CModalFooter,
-  CModalBody,
-  CModalCloseButton,
-  CIconButton,
-  CFlex,
-  CHeading,
-  CFormControl,
-  CFormLabel,
-  CFormErrorMessage,
-  CFormHelperText,
-  CInput,
-} from '@chakra-ui/vue'
+<script setup lang="ts">
 import draw from '../script/draw.js';
 import showResults from '../script/drawResults.js';
-export default {
-  name: 'IndexPage',
-  components: {
-    CBox,
-    CButton,
-    CModal,
-    CModalContent,
-    CModalOverlay,
-    CModalHeader,
-    CModalFooter,
-    CModalBody,
-    CModalCloseButton,
-    CIconButton,
-    CFlex,
-    CHeading
-  },
-  inject: ['$chakraColorMode', '$toggleColorMode'],
-  data () {
-    return {
-      showModalLottery: false,
-      showModalResult: false,
-      startLottery: false,
-      lotteryFinished: false, 
-      results: [],
-      person: '',
-      drawPick: '',
-      mainStyles: {
-        dark: {
-          bg: 'gray.700',
-          color: 'whiteAlpha.900'
-        },
-        light: {
-          bg: 'white',
-          color: 'gray.900'
-        }
-      }
+  
+  const showModalLottery = ref(false);
+  const showModalResult = ref(false);
+  const startLottery = ref(false);
+  const lotteryFinished = ref(false); 
+  const results = ref([]);
+  const person = ref('');
+  const drawPick = ref('');
+  const mainStyles = ref({
+    dark: {
+      bg: 'gray.700',
+      color: 'whiteAlpha.900'
+    },
+    light: {
+      bg: 'white',
+      color: 'gray.900'
     }
-  },
-  computed: {
-    colorMode () {
-      return this.$chakraColorMode()
-    },
-    theme () {
-      return this.$chakraTheme()
-    },
-    toggleColorMode () {
-      return this.$toggleColorMode
-    },
-  },
-  methods: {
-    onClickSubmitHandler() {
-      this.drawPick = showResults(this.results, this.person);
-      this.showModalResult = true;
-    },
-    onClickStartLotteryHandler(startLottery) {      
-      if (!startLottery) {
-        this.showModalLottery = true
-      } else {
-        this.results= [];
-        this.startLottery = true;
-        this.results = draw();        
-      }
-    },
-    setName(e) {
-      this.person = e.target.value;
+  })
+
+  const onClickSubmitHandler = () => {
+    drawPick.value = showResults(results, person);
+    showModalResult.value = true;
+  };
+
+  const onClickStartLotteryHandler = (startLottery: boolean = false) => {      
+    if (!startLottery) {
+      showModalLottery.value = true
+    } else {
+      results.value = [];
+      startLottery = true;
+      results.value = draw();        
     }
+  };
+
+  const setName = (e) => {
+    person.value = e.target.value;
   }
-}
 </script>
